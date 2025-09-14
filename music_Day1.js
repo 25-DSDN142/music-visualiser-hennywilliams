0c99bffb508bc08e424027d44fc6014e6178f1ea
hennywilliams <hennywilliamsnz@gmail.com>
2025-09-14 21:22:47 +1200

    Main image added

--- music_Day1.js
+++ music_Day1.js
@@ -1,8 +1,16 @@
-
+let firstRun = true;
+let myImage; 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
 background(222, 173, 207); //pink main background
 
+if(firstRun) {
+myImage = loadImage('A_boombox1.png')
 
+firstRun = false;
+
+}
+
+image(myImage,260,110);
 
 }
