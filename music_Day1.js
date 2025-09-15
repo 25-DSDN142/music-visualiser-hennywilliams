fa6909208fd8516359d2524c047176876d33c650
hennywilliams <hennywilliamsnz@gmail.com>
2025-09-15 15:31:56 +1200

    Added music bars + comments + small details

--- music_Day1.js
+++ music_Day1.js
@@ -2,20 +2,29 @@ let firstRun = true;
 let myImage;
 let myImage2;
 var Ymove = 1; 
-// vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
 background(222, 173, 207); //pink main background
-
 if(firstRun) {
 myImage = loadImage('A_boombox1.png') //bottom right side image
-myImage2 = loadImage('Retroflower2.png') 
+myImage2 = loadImage('Retroflower2.png') //back flower image
 firstRun = false;
 
 }
 
+let boomboxY = map(drum,0,100,60,100);
+image(myImage2,110,0);
 
-image(myImage2,60,30);
-image(myImage,260,110);
+fill(252, 184, 88);
+ellipse(331,-20+Ymove,5);
+ellipse(463,-50+Ymove,8);
+ellipse(400,-70+Ymove,12);
+
+let rect1Y = map(bass,0,100,250,400);
+fill(166, 245, 154,80);
+rect(80,rect1Y,80,270,20);
+rect(170,250,80,270,20);
+rect(260,250,80,270,20);
+image(myImage,260,boomboxY);
 
 noStroke();
 fill(232, 223, 139);
@@ -26,13 +35,6 @@ if (Ymove > 640){
 Ymove = 0;
 
 }
-
-fill(252, 184, 88);
-ellipse(331,-20+Ymove,5);
-ellipse(463,-50+Ymove,8);
-ellipse(400,-70+Ymove,12);
-
-
 
 
 
